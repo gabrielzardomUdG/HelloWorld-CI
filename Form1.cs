a23d2906bd82dedb3c907de0dae9e079933a5059
Gabriel Ángel Zárate Domínguez <gabriel.zarate5909@alumnos.udg.mx>
2025-09-27 13:28:00 -0600

    +Adding some comments to the code

--- Form1.cs
+++ Form1.cs
@@ -4,14 +4,24 @@ namespace HolaMundo
 {
     public partial class Form1 : Form
     {
-        private string pattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^a-zA-Z0-9]).+$";
-        private string MESSAGE_PASSWORD_ERROR = "Password doesnt comply with the listed requirements!";
+
+        /*
+         Some constants to avoid lengthy lines.
+         */
+
+        const string pattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^a-zA-Z0-9]).+$";
+        const string MESSAGE_PASSWORD_ERROR = "Password doesnt comply with the listed requirements!";
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        /*
+        When cliking on the button confirm, this function will run, first it will check if the RegExp pattern is matched,
+        then it will check if both passwords are the same, if any of these checks fail, an error message will be shown,
+        otherwise a success message will be shown.
+        */
         private void btn_confirm_Click(object sender, EventArgs e)
         {
             if(!Regex.IsMatch(txt_password.Text, pattern)) 
